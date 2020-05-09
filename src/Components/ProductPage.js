0efce5c8caebc98d0aca747e5a653082bf9ba2e2
Amr Sekilly <amr@pelcro.com>
2020-05-09 22:06:49 +0200

    Added react hooks

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -1,39 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Loader from "react-loader-spinner";
 import { get as axiosGet } from "axios";
 
-export class ProductPage extends Component {
-  state = {
-    name: "",
-    price: "",
-    isLoaded: false,
-  };
-  componentDidMount() {
+export function ProductPage() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
     let pathname = window.location.pathname.substr(1).split("/")[1];
     axiosGet(`${process.env.REACT_APP_BASE_URL}/products/${pathname}`).then(
-      ({ data: { name, price } }) =>
-        this.setState({
-          name,
-          price,
-          isLoaded: true,
-        })
+      ({ data: { name, price } }) => {
+        setName(name);
+        setPrice(price);
+        setIsLoaded(true);
+      }
     );
-  }
+  });
 
-  render() {
-    const { name, price, isLoaded } = this.state;
-    if (isLoaded) {
-      return (
-        <div>
-          <h1>Product: {name}</h1>
-          <b>Price: ${price}</b>
-        </div>
-      );
-    }
+  if (isLoaded) {
     return (
-      <div style={{ display: "block", margin: "auto" }}>
-        <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+      <div>
+        <h1>Product: {name}</h1>
+        <b>Price: ${price}</b>
       </div>
     );
   }
+  return (
+    <div style={{ display: "block", margin: "auto" }}>
+      <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
+    </div>
+  );
 }
