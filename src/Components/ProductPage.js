02227b417f4c8479d582f7cc79a0225203c0d655
Amr Sekilly <amr@pelcro.com>
2020-05-09 22:15:21 +0200

    Practiced the hooks

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -6,8 +6,10 @@ export function ProductPage() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [isLoaded, setIsLoaded] = useState(false);
+  const [count, setCount] = useState(0);
 
-  useEffect(() => {
+  // Similar to ComponentDidMount
+  useEffect(function () {
     let pathname = window.location.pathname.substr(1).split("/")[1];
     axiosGet(`${process.env.REACT_APP_BASE_URL}/products/${pathname}`).then(
       ({ data: { name, price } }) => {
@@ -16,13 +18,20 @@ export function ProductPage() {
         setIsLoaded(true);
       }
     );
-  });
+
+    // ComponentWillUnmount
+    return function () {
+      console.log("Component will unmount");
+    };
+  }, []);
 
   if (isLoaded) {
     return (
       <div>
         <h1>Product: {name}</h1>
         <b>Price: ${price}</b>
+        <p>Counter: {count}</p>
+        <button onClick={() => setCount(count + 1)}>+1</button>
       </div>
     );
   }
